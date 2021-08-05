73688ad11cc89348e7deacb317b3948108c848b8
adamkpl <50586694+adamkpl@users.noreply.github.com>
2021-08-05 16:57:47 +0200

    Finalize ReverseString

--- src/main/java/workbook/ReverseString.java
+++ src/main/java/workbook/ReverseString.java
@@ -6,31 +6,38 @@ public class ReverseString {
     public static void main(String[] args) {
         // Take input - a single String
         String input = "kawa";
+        System.out.println("input: " + input);
 
-        // Reverse and Print
-        System.out.println(reverseString(input));
+        // Reverse
+        String inputReversed = reverseString(input);
+
+        // and Print
+        System.out.println("output: " + inputReversed);
 
         // Take input - a list of Strings.
         ArrayList<String> strings = new ArrayList<>();
         strings.add("10");
-        strings.add("adam");
-        strings.add("Kayak");
+        strings.add("20");
+        strings.add("30");
+        System.out.println("strings: " + strings);
 
-        // Reverse and Print
-        reverseAndPrintStrings(strings);
+        // Reverse
+        ArrayList<String> stringsReversed = reverseString(strings);
+
+        // and Print
+        System.out.println("stringsReversed: " + stringsReversed);
 
     }
 
     private static String reverseString(String s) {
-        s = new StringBuilder(s).reverse().toString();
-        return s;
+        return new StringBuilder(s).reverse().toString();
     }
 
-    private static void reverseAndPrintStrings(ArrayList<String> s) {
-        for (String reversedString:s) {
-            reversedString = new StringBuilder(reversedString).reverse().toString();
-            System.out.println(reversedString);
+    private static ArrayList<String> reverseString(ArrayList<String> s) {
+        for (int i = 0; i < s.size(); i++) {
+            s.set(i, reverseString(s.get(i)));
         }
+        return s;
     }
 
 }
